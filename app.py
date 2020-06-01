a409b2a9015d5576b471266216e3ac4a58b0c31b
richard <richard.deroure@gmail.com>
2020-06-01 16:56:07 +0100

    added registration functionality with tests

--- app.py
+++ app.py
@@ -1,6 +1,35 @@
-from flask import Flask, make_response, render_template
+from flask import Flask, make_response, render_template, g, redirect, url_for, flash, request
+from werkzeug.security import check_password_hash, generate_password_hash
+import sqlite3 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'rajsiosorqwnejrq39834tergm4'
+app.config['DATABASE'] = 'flaskr.db'
+app.config['DEBUG'] = True
+
+# connect to database
+def connect_db():
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+# create the database
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+# open database connection
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+# close database connection
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
 
 @app.route('/')
 def home():
@@ -10,8 +39,33 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route('/register')
+@app.route('/register', methods=('GET', 'POST'))
 def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = 'User {} is already \
+                   registered.'.format(username)
+
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES \
+               (?, ?)', (username, generate_password_hash(password))
+            )
+            db.commit()
+            return redirect(url_for('login'))
+
+        flash(error)
+
     return render_template('register.html')
 
 @app.route('/login')
